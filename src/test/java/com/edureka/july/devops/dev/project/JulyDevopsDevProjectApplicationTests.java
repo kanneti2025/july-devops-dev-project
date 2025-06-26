aae4b5d42be814bec68e5dc95a9b1f3038fa8b9f
Ramesh A <ramesharpu@gmail.com>
2025-06-26 07:23:18 +0530

    initial commit

---
+++ src/test/java/com/edureka/july/devops/dev/project/JulyDevopsDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.july.devops.dev.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyDevopsDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
